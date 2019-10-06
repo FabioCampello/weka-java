e24f4f2b4c6cc9384eb58ddeb31b70247ff43c24
Fabio Francisco Campelo <fabiocampello@outlook.com.br>
2019-10-05 22:28:07 -0300

    SAC03 - Implementada recuperação da base de dados.

--- src/br/com/weka/java/ApplicationMain.java
+++ src/br/com/weka/java/ApplicationMain.java
@@ -1,9 +1,30 @@
 package br.com.weka.java;
 
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+/**
+ * @author fabio
+ *
+ */
 public class ApplicationMain {
 	
+	static DataSource ds;
+	static Instances ins;
+	
 	public static void main(String[] args) {
-		
+		try {
+			ds = new DataSource("src/br/com/weka/java/vendas.arff");
+			ins = recuperaBaseDeDados(ds);
+			System.out.println(ins.toString());
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	static Instances recuperaBaseDeDados(DataSource data) throws Exception {
+		Instances instance = data.getDataSet();
+		return instance;
 	}
 
 }
