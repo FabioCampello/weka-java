ba9db4f5ad25f9ff0284cfc26bcb8861c1ea8dae
Fabio Francisco Campelo <fabiocampello@outlook.com.br>
2019-10-06 03:22:26 -0300

    SAC03 - Finalizado aplicação de algoritmo de classificação.

--- src/br/com/weka/java/ApplicationMain.java
+++ src/br/com/weka/java/ApplicationMain.java
@@ -1,5 +1,8 @@
 package br.com.weka.java;
 
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -11,12 +14,16 @@ public class ApplicationMain {
 	
 	static DataSource ds;
 	static Instances ins;
+	static NaiveBayes nb;
 	
 	public static void main(String[] args) {
 		try {
 			ds = new DataSource("src/br/com/weka/java/vendas.arff");
 			ins = recuperaBaseDeDados(ds);
-			System.out.println(ins.toString());
+			
+			ins.setClassIndex(3);
+			
+			aplicandoNaiveBayes(ins);
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -25,6 +32,24 @@ public class ApplicationMain {
 	static Instances recuperaBaseDeDados(DataSource data) throws Exception {
 		Instances instance = data.getDataSet();
 		return instance;
+	}
+	
+	static void aplicandoNaiveBayes(Instances i) throws Exception {
+		nb = new NaiveBayes();
+		nb.buildClassifier(i);
+		setNewInstance(i);
+	}
+	
+	static void setNewInstance(Instances i) throws Exception {
+		Instance novo = new DenseInstance(4);
+		novo.setDataset(i);
+		novo.setValue(0, "M");
+		novo.setValue(1, "20-39");
+		novo.setValue(2, "Nao");
+		
+		double probabilidade[] = nb.distributionForInstance(novo);
+		System.out.println("Sim: " + probabilidade[1]);
+		System.out.println("Não: " + probabilidade[0]);
 	}
 
 }
